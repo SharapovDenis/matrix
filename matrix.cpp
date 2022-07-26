46f16c9ce0d558e96b502e395ddf511730e784d1
SharapovDenis <sharapov.d@phystech.edu>
2022-07-26 23:07:56 +0300

    slow matrix

--- matrix.cpp
+++ matrix.cpp
@@ -1,401 +1,377 @@
-#include <iostream>
-#include <utility>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <fstream>
-#include <sstream>
+#include "matrix.hpp"
 
 static bool abs_compare(int a, int b) {
     return (std::abs(a) < std::abs(b));
 }
 
-class Matrix {
+Matrix::Matrix() : rows(0), cols(0), m(nullptr) {}
 
-private:
-
-    size_t rows;
-    size_t cols;
-    std::vector<std::vector<double>> *m;
-
-public:
-
-    Matrix() : rows(0), cols(0), m(nullptr) {}
-
-    Matrix(size_t rows, size_t cols) : rows(rows), cols(cols) {
-        m = new std::vector<std::vector<double>>(rows, std::vector<double>(cols));
-        for (auto i = 0; i < rows; ++i) {
-            for (auto j = 0; j < cols; ++j) {
-                m->at(i)[j] = 0;
-            }
+Matrix::Matrix(size_t rows, size_t cols) : rows(rows), cols(cols) {
+    m = new std::vector<std::vector<double>>(rows, std::vector<double>(cols));
+    for (auto i = 0; i < rows; ++i) {
+        for (auto j = 0; j < cols; ++j) {
+            m->at(i)[j] = 0;
         }
     }
+}
 
-    Matrix(const Matrix &M) {
-        rows = M.rows;
-        cols = M.cols;
+Matrix::Matrix(const Matrix &M) {
+    rows = M.rows;
+    cols = M.cols;
 
-        m = new std::vector<std::vector<double>>(rows, std::vector<double>(cols));
+    m = new std::vector<std::vector<double>>(rows, std::vector<double>(cols));
 
-        for (auto i = 0; i < rows; ++i) {
-            for (auto j = 0; j < cols; ++j) {
-                m->at(i)[j] = M[i][j];
-            }
+    for (auto i = 0; i < rows; ++i) {
+        for (auto j = 0; j < cols; ++j) {
+            m->at(i)[j] = M[i][j];
         }
     }
+}
 
-    explicit Matrix(const std::vector<std::vector<double>> &ptr) {
-        rows = ptr.size();
+Matrix::Matrix(const std::vector<std::vector<double>> &ptr) {
+    rows = ptr.size();
 
-        if (rows != 0) {
-            cols = ptr[0].size();
-        } else {
-            rows = 0;
-        }
-
-        m = new std::vector<std::vector<double>>(rows, std::vector<double>(cols));
-
-        for (auto i = 0; i < rows; ++i) {
-            for (auto j = 0; j < cols; ++j) {
-                m->at(i)[j] = ptr[i][j];
-            }
-        }
+    if (rows != 0) {
+        cols = ptr[0].size();
+    } else {
+        rows = 0;
     }
 
-    ~Matrix() {
+    m = new std::vector<std::vector<double>>(rows, std::vector<double>(cols));
+
+    for (auto i = 0; i < rows; ++i) {
+        for (auto j = 0; j < cols; ++j) {
+            m->at(i)[j] = ptr[i][j];
+        }
+    }
+}
+
+Matrix::~Matrix() {
 //        m->clear();
-        delete m;
-    }
+    delete m;
+}
 
-    Matrix &operator=(const Matrix &M) {
+Matrix &Matrix::operator=(const Matrix &M) {
 
-        if (this == &M) {
-            return *this;
-        }
-
-        delete m;
-
-        rows = M.rows;
-        cols = M.cols;
-        m = new std::vector<std::vector<double>>(rows, std::vector<double>(cols));
-
-        for (auto i = 0; i < rows; ++i) {
-            for (auto j = 0; j < cols; ++j) {
-                m->at(i)[j] = M[i][j];
-            }
-        }
-
+    if (this == &M) {
         return *this;
     }
 
-    Matrix &operator=(Matrix &&M) noexcept {
+    delete m;
 
-        if (this == &M) {
-            return *this;
+    rows = M.rows;
+    cols = M.cols;
+    m = new std::vector<std::vector<double>>(rows, std::vector<double>(cols));
+
+    for (auto i = 0; i < rows; ++i) {
+        for (auto j = 0; j < cols; ++j) {
+            m->at(i)[j] = M[i][j];
         }
-
-        delete m;
-
-        m = std::exchange(M.m, nullptr);
-        rows = std::exchange(M.rows, 0);
-        cols = std::exchange(M.cols, 0);
-
-        return *this;
-
     }
 
-    Matrix &operator+=(const Matrix &M) {
+    return *this;
+}
 
-        if (rows != M.rows) {
-            std::cerr << "summarize incorrect shapes" << std::endl;
-            std::exit(-1);
-        }
+Matrix &Matrix::operator=(Matrix &&M) noexcept {
 
-        if (cols != M.cols) {
-            std::cerr << "summarize incorrect shapes" << std::endl;
-            std::exit(-1);
-        }
-
-        for (auto i = 0; i < rows; ++i) {
-            for (auto j = 0; j < cols; ++j) {
-                m->at(i)[j] += M[i][j];
-            }
-        }
-
+    if (this == &M) {
         return *this;
     }
 
-    friend const Matrix operator+(Matrix A, const Matrix &B) { // NOLINT(readability-const-return-type)
-        A += B;
-        return A;
+    delete m;
+
+    m = std::exchange(M.m, nullptr);
+    rows = std::exchange(M.rows, 0);
+    cols = std::exchange(M.cols, 0);
+
+    return *this;
+
+}
+
+Matrix &Matrix::operator+=(const Matrix &M) {
+
+    if (rows != M.rows) {
+        std::cerr << "summarize incorrect shapes" << std::endl;
+        std::exit(-1);
     }
 
-    Matrix &operator-=(const Matrix &M) {
+    if (cols != M.cols) {
+        std::cerr << "summarize incorrect shapes" << std::endl;
+        std::exit(-1);
+    }
 
-        if (rows != M.rows) {
-            std::cerr << "summarize incorrect shapes" << std::endl;
-            std::exit(-1);
+    for (auto i = 0; i < rows; ++i) {
+        for (auto j = 0; j < cols; ++j) {
+            m->at(i)[j] += M[i][j];
         }
+    }
 
-        if (cols != M.cols) {
-            std::cerr << "summarize incorrect shapes" << std::endl;
-            std::exit(-1);
+    return *this;
+}
+
+const Matrix operator+(Matrix A, const Matrix &B) { // NOLINT(readability-const-return-type)
+    A += B;
+    return A;
+}
+
+Matrix &Matrix::operator-=(const Matrix &M) {
+
+    if (rows != M.rows) {
+        std::cerr << "summarize incorrect shapes" << std::endl;
+        std::exit(-1);
+    }
+
+    if (cols != M.cols) {
+        std::cerr << "summarize incorrect shapes" << std::endl;
+        std::exit(-1);
+    }
+
+    for (auto i = 0; i < rows; ++i) {
+        for (auto j = 0; j < cols; ++j) {
+            m->at(i)[j] -= M[i][j];
         }
+    }
 
-        for (auto i = 0; i < rows; ++i) {
-            for (auto j = 0; j < cols; ++j) {
-                m->at(i)[j] -= M[i][j];
+    return *this;
+}
+
+const Matrix operator-(Matrix A, const Matrix &B) { // NOLINT(readability-const-return-type)
+    A -= B;
+    return A;
+}
+
+Matrix &Matrix::operator*=(const Matrix &M) {
+
+    if (cols != M.rows) {
+        std::cerr << "multiply incorrect shapes" << std::endl;
+        std::exit(-1);
+    }
+
+    Matrix tmp(rows, M.cols);
+
+    for (auto i = 0; i < rows; ++i) {
+        for (auto j = 0; j < M.cols; ++j) {
+            for (auto r = 0; r < M.rows; ++r) {
+                tmp[i][j] += m->at(i)[r] * M[r][j];
             }
         }
+    }
+    *this = tmp;
 
-        return *this;
+    return *this;
+}
+
+const Matrix operator*(Matrix A, const Matrix &B) { // NOLINT(readability-const-return-type)
+
+    if (A.cols != B.rows) {
+        std::cerr << "multiply incorrect shapes" << std::endl;
+        std::exit(-1);
     }
 
-    friend const Matrix operator-(Matrix A, const Matrix &B) { // NOLINT(readability-const-return-type)
-        A -= B;
-        return A;
-    }
+    A *= B;
 
-    Matrix &operator*=(const Matrix &M) {
+    return A;
+}
 
-        if (cols != M.rows) {
-            std::cerr << "multiply incorrect shapes" << std::endl;
-            std::exit(-1);
-        }
+std::vector<double> &Matrix::operator[](int i) {
+    return m->at(i);
+}
 
-        Matrix tmp(rows, M.cols);
+const std::vector<double> &Matrix::operator[](int i) const {
+    return m->at(i);
+}
 
-        for (auto i = 0; i < rows; ++i) {
-            for (auto j = 0; j < M.cols; ++j) {
-                for (auto r = 0; r < M.rows; ++r) {
-                    tmp[i][j] += m->at(i)[r] * M[r][j];
-                }
-            }
-        }
-        *this = tmp;
+std::ostream &operator<<(std::ostream &os, const Matrix &M) {
 
-        return *this;
-    }
-
-    friend const Matrix operator*(Matrix A, const Matrix &B) { // NOLINT(readability-const-return-type)
-
-        if (A.cols != B.rows) {
-            std::cerr << "multiply incorrect shapes" << std::endl;
-            std::exit(-1);
-        }
-
-        A *= B;
-
-        return A;
-    }
-
-    std::vector<double> &operator[](int i) {
-        return m->at(i);
-    }
-
-    const std::vector<double> &operator[](int i) const {
-        return m->at(i);
-    }
-
-    friend std::ostream &operator<<(std::ostream &os, const Matrix &M) {
-
-        if (M.rows == 0 && M.cols == 0) {
-            std::cout << "empty matrix!" << std::endl;
-            return os;
-        }
-
-        for (auto i = 0; i < M.rows; ++i) {
-            for (auto j = 0; j < M.cols; ++j) {
-                std::cout << M.m->at(i)[j] << " ";
-            }
-            std::cout << std::endl;
-        }
-
+    if (M.rows == 0 && M.cols == 0) {
+        std::cout << "empty matrix!" << std::endl;
         return os;
     }
 
-    virtual bool is_empty() {
-        if (rows == 0 && cols == 0)
-            return true;
-        else
-            return false;
+    for (auto i = 0; i < M.rows; ++i) {
+        for (auto j = 0; j < M.cols; ++j) {
+            std::cout << M.m->at(i)[j] << " ";
+        }
+        std::cout << std::endl;
     }
 
-    void print() {
+    return os;
+}
 
-        if (rows == 0 && cols == 0) {
-            std::cout << "empty matrix!" << std::endl;
-            return;
+bool Matrix::is_empty() {
+    if (rows == 0 && cols == 0)
+        return true;
+    else
+        return false;
+}
+
+void Matrix::print() {
+
+    if (rows == 0 && cols == 0) {
+        std::cout << "empty matrix!" << std::endl;
+        return;
+    }
+
+    for (auto i = 0; i < rows; ++i) {
+        for (auto j = 0; j < cols; ++j) {
+            std::cout << m->at(i)[j] << " ";
         }
+        std::cout << std::endl;
+    }
+}
+
+std::pair<size_t, size_t> Matrix::shape() {
+    return std::make_pair(rows, cols);
+}
+
+Matrix &Matrix::colchange(Matrix &col, size_t index) {
+    // Can change col of any size except the size that is bigger than M.rows
+
+    if (col.cols != 1) {
+        std::cerr << "colchange(): incorrect col shape" << std::endl;
+        std::exit(-1);
+    }
+
+    if (col.rows > this->rows) {
+        std::cerr << "colchange(): col.rows is bigger than this->rows" << std::endl;
+        std::exit(-1);
+    }
+
+    if (index > this->cols) {
+        std::cerr << "colchange(): index is bigger than this->rows" << std::endl;
+        std::exit(-1);
+    }
+
+    for (auto i = 0; i < col.rows; ++i) {
+        m->at(i)[index] = col[i][0];
+    }
+
+    return *this;
+}
+
+Matrix Matrix::getcols(size_t from, size_t upto) {
+
+    if (upto > cols) {
+        std::cerr << "getcols(): upto > this->rows" << std::endl;
+        std::exit(-1);
+    }
+
+    if (upto < from) {
+        std::cerr << "getcols(): upto < from" << std::endl;
+        std::exit(-1);
+    }
+
+    if (from == upto) {
+        size_t index = from;
+        Matrix M(rows, 1);
 
         for (auto i = 0; i < rows; ++i) {
-            for (auto j = 0; j < cols; ++j) {
-                std::cout << m->at(i)[j] << " ";
-            }
-            std::cout << std::endl;
-        }
-    }
-
-    std::pair<size_t, size_t> shape() {
-        return std::make_pair(rows, cols);
-    }
-
-    Matrix &colchange(Matrix &col, size_t index) {
-        // Can change col of any size except the size that is bigger than M.rows
-
-        if (col.cols != 1) {
-            std::cerr << "colchange(): incorrect col shape" << std::endl;
-            std::exit(-1);
-        }
-
-        if (col.rows > this->rows) {
-            std::cerr << "colchange(): col.rows is bigger than this->rows" << std::endl;
-            std::exit(-1);
-        }
-
-        if (index > this->cols) {
-            std::cerr << "colchange(): index is bigger than this->rows" << std::endl;
-            std::exit(-1);
-        }
-
-        for (auto i = 0; i < col.rows; ++i) {
-            m->at(i)[index] = col[i][0];
-        }
-
-        return *this;
-    }
-
-    friend Matrix eye(size_t s);
-
-    friend Matrix hconcat(Matrix &A, Matrix &B);
-
-    friend Matrix vconcat(Matrix &A, Matrix &B);
-
-    Matrix getcols(size_t from, size_t upto) {
-
-        if (upto > cols) {
-            std::cerr << "getcols(): upto > this->rows" << std::endl;
-            std::exit(-1);
-        }
-
-        if (upto < from) {
-            std::cerr << "getcols(): upto < from" << std::endl;
-            std::exit(-1);
-        }
-
-        if (from == upto) {
-            size_t index = from;
-            Matrix M(rows, 1);
-
-            for (auto i = 0; i < rows; ++i) {
-                M[i][0] = m->at(i)[index];
-            }
-
-            return M;
-        }
-
-        Matrix M(rows, upto - from);
-
-        for (auto i = 0; i < M.rows; ++i) {
-            for (auto j = from; j < upto; ++j) {
-                M[i][j - from] = m->at(i)[j];
-            }
+            M[i][0] = m->at(i)[index];
         }
 
         return M;
     }
 
-    Matrix slice(size_t from, size_t upto, size_t index) {
+    Matrix M(rows, upto - from);
 
-        if (index > cols) {
-            std::cerr << "slice(): index > this->rows" << std::endl;
-            std::exit(-1);
+    for (auto i = 0; i < M.rows; ++i) {
+        for (auto j = from; j < upto; ++j) {
+            M[i][j - from] = m->at(i)[j];
         }
-
-        if (upto > rows) {
-            std::cerr << "slice(): upto > rows" << std::endl;
-            std::exit(-1);
-        }
-
-        if (upto < from) {
-            std::cerr << "slice(): upto < from" << std::endl;
-            std::exit(-1);
-        }
-
-        if (upto == from) {
-            Matrix ret;
-            return ret;
-        }
-
-        Matrix mid(upto - from, 1);
-
-        for (size_t i = from; i < upto; ++i) {
-            mid[i - from][0] = m->at(i)[index];
-        }
-
-        return mid;
     }
 
-    double maxval() {
+    return M;
+}
 
-        if (this->is_empty()) {
-            std::cerr << "maxval(): empty matrix!" << std::endl;
-            std::exit(-1);
-        }
+Matrix Matrix::slice(size_t from, size_t upto, size_t index) {
 
-        std::vector<double> max_values;
-        std::vector<double>::iterator result;
-        size_t index;
-
-        for (auto i = 0; i < rows; ++i) {
-            result = std::max_element(m->at(i).begin(), m->at(i).end(), abs_compare);
-            index = std::distance(m->at(i).begin(), result);
-            max_values.push_back(m->at(i)[index]);
-        }
-
-        result = std::max_element(max_values.begin(), max_values.end(), abs_compare);
-        index = std::distance(max_values.begin(), result);
-
-        return max_values[index];
+    if (index > cols) {
+        std::cerr << "slice(): index > this->rows" << std::endl;
+        std::exit(-1);
     }
 
-    void read_csv(const char *filename, const char delim) {
-
-        std::ifstream file;
-        file.open(filename, std::ifstream::in);
-
-        if (!file.is_open()) {
-            std::cerr << "error while opening the file " << filename << std::endl;
-            std::exit(-1);
-        }
-
-        if (m == nullptr)
-            m = new std::vector<std::vector<double>>;
-
-        std::string line, word;
-        std::vector<double> row;
-
-        while (file >> line) {
-
-            std::stringstream s(line);
-            row.clear();
-
-            while (std::getline(s, word, delim)) {
-
-                if (!word.empty())
-                    row.push_back(std::stod(word));
-            }
-            m->push_back(row);
-        }
-
-        rows = m->size();
-        cols = row.size();
-
-        file.close();
-
+    if (upto > rows) {
+        std::cerr << "slice(): upto > rows" << std::endl;
+        std::exit(-1);
     }
 
-};
+    if (upto < from) {
+        std::cerr << "slice(): upto < from" << std::endl;
+        std::exit(-1);
+    }
+
+    if (upto == from) {
+        Matrix ret;
+        return ret;
+    }
+
+    Matrix mid(upto - from, 1);
+
+    for (size_t i = from; i < upto; ++i) {
+        mid[i - from][0] = m->at(i)[index];
+    }
+
+    return mid;
+}
+
+double Matrix::maxval() {
+
+    if (this->is_empty()) {
+        std::cerr << "maxval(): empty matrix!" << std::endl;
+        std::exit(-1);
+    }
+
+    std::vector<double> max_values;
+    std::vector<double>::iterator result;
+    size_t index;
+
+    for (auto i = 0; i < rows; ++i) {
+        result = std::max_element(m->at(i).begin(), m->at(i).end(), abs_compare);
+        index = std::distance(m->at(i).begin(), result);
+        max_values.push_back(m->at(i)[index]);
+    }
+
+    result = std::max_element(max_values.begin(), max_values.end(), abs_compare);
+    index = std::distance(max_values.begin(), result);
+
+    return max_values[index];
+}
+
+void Matrix::read_csv(const char *filename, const char delim) {
+
+    std::ifstream file;
+    file.open(filename, std::ifstream::in);
+
+    if (!file.is_open()) {
+        std::cerr << "error while opening the file " << filename << std::endl;
+        std::exit(-1);
+    }
+
+    if (m == nullptr)
+        m = new std::vector<std::vector<double>>;
+
+    std::string line, word;
+    std::vector<double> row;
+
+    while (file >> line) {
+
+        std::stringstream s(line);
+        row.clear();
+
+        while (std::getline(s, word, delim)) {
+
+            if (!word.empty())
+                row.push_back(std::stod(word));
+        }
+        m->push_back(row);
+    }
+
+    rows = m->size();
+    cols = row.size();
+
+    file.close();
+
+}
 
 Matrix eye(size_t s) {
 
@@ -472,6 +448,8 @@ int main() {
 
     Matrix A;
     A.read_csv("table_3_1.csv", ',');
+    std::cout << A.shape().first << " " << A.shape().second << std::endl;
+    A *= A;
 
 }
 
